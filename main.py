b34e45aab277403aa51e55e031f513efae925feb
Matt Bremmers <bremmdev@gmail.com>
2024-01-08 23:24:52 +0100

    feat: keyboard accelerators for file menu

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 import tkinter as tk
-from file import open_file, new_file, save_file, save_file_as
+from widgets import create_topbar
 
 
 def main():
     window = tk.Tk()
     window.title('Untitled - breditor')
 
-    # Create a topbar
-    topbar = tk.Menu(window)
-    window.config(menu=topbar)
-
-    # Create a submenu for File
-    file_menu = tk.Menu(topbar, tearoff=0)
-    topbar.add_cascade(label="File", menu=file_menu)
-
-    file_menu.add_command(
-        label="New", command=lambda: new_file(window, edit_text))
-    file_menu.add_command(
-        label="Open", command=lambda: open_file(window, edit_text))
-    file_menu.add_command(
-        label="Save", command=lambda: save_file(window, edit_text))
-    file_menu.add_command(
-        label="Save As", command=lambda: save_file_as(window, edit_text))
-
     # create a text field
     edit_text = tk.Text(window)
     edit_text.pack(expand=True, fill=tk.BOTH)
+
+    create_topbar(window, edit_text)
 
     window.mainloop()
 
